6a339121c839d7fb5dfa5c6dc402c444940630cd
tprodanov <timofey.prodanov@gmail.com>
2016-03-12 13:13:16 +0300

    Fixed

--- 2/hmm.py
+++ 2/hmm.py
@@ -2,15 +2,34 @@ import math
 import eln
 import sys
 
+
 class Model:
 
     def __init__(self, hidden_states, emission_states):
         self.hidden_states = hidden_states
         self.emission_states = emission_states
 
-        self.initial = [0.0] * self.hidden_states
-        self.transitions = [[0.0] * self.hidden_states for _ in range(self.hidden_states)]
-        self.emission = [[0.0] * self.emission_states for _ in range(self.hidden_states)]
+        self.initial = [math.nan] * self.hidden_states
+        self.transitions = [[math.nan] * self.hidden_states for _ in range(self.hidden_states)]
+        self.emission = [[math.nan] * self.emission_states for _ in range(self.hidden_states)]
+
+    def normalize(self):
+        initial_sum = math.nan
+        for initial in self.initial:
+            initial_sum = eln.sum(initial_sum, initial)
+        self.initial = [eln.product(initial, -initial_sum) for initial in self.initial]
+
+        for i in range(self.hidden_states):
+            transitions_sum = math.nan
+            for transition in self.transitions[i]:
+                transitions_sum = eln.sum(transitions_sum, transition)
+            self.transitions[i] = [eln.product(transition, -transitions_sum) for transition in self.transitions[i]]
+
+        for i in range(self.hidden_states):
+            emissions_sum = math.nan
+            for emission in self.emission[i]:
+                emissions_sum = eln.sum(emissions_sum, emission)
+            self.emission[i] = [eln.product(emission, -emissions_sum) for emission in self.emission[i]]
 
     @staticmethod
     def load(file):
@@ -31,6 +50,7 @@ class Model:
         return model
 
     def print(self, file):
+
         file.write('Initial:\n')
         for initial in self.initial:
             file.write('\t%f' % eln.exp(initial))
@@ -41,6 +61,7 @@ class Model:
             for transition in outer:
                 file.write('\t%f' % eln.exp(transition))
             file.write('\n')
+
         file.write('Emission:\n')
         for outer in self.emission:
             for emission in outer:
@@ -84,7 +105,7 @@ class Model:
         transitions_sum = [sum(row) for row in transitions]
         for i in range(self.hidden_states):
             for j in range(self.hidden_states):
-                self.transitions[i][j] = float(transitions[i][j]) / transitions_sum[i]
+                self.transitions[i][j] = eln.ln(float(transitions[i][j]) / transitions_sum[i])
 
     def backward_forward(self, observations):
         n = len(observations)
@@ -117,21 +138,9 @@ class Model:
             for j in range(self.hidden_states):
                 gamma[t][j] = eln.product(eln.product(alpha[t][j], beta[t][j]), -sequence_probability)
 
-        return alpha, beta, gamma
+        return alpha, beta, gamma, sequence_probability
 
-    def baum_welch(self, observations, alpha, beta, gamma):
-        print('-------------')
-        print('-------------')
-        self.print(sys.stdout)
-
-        eln.print_matrix(alpha, 'alpha')
-        eln.print_matrix(beta, 'beta')
-        eln.print_matrix(gamma, 'gamma')
-
-        sequence_probability = math.nan
-        for i in range(self.hidden_states):
-            sequence_probability = eln.sum(sequence_probability, alpha[-1][i])
-
+    def baum_welch(self, observations, alpha, beta, gamma, sequence_probability):
         n = len(observations)
         xi = [[[math.nan] * self.hidden_states for _ in range(self.hidden_states)] for _ in range(n - 1)]
         for t in range(n - 1):
@@ -140,16 +149,12 @@ class Model:
                     xi[t][i][j] = eln.product(eln.product(alpha[t][i], self.transitions[i][j]),
                                               eln.product(beta[t + 1][j], self.emission[j][observations[t + 1]]))
                     xi[t][i][j] = eln.product(xi[t][i][j], -sequence_probability)
-
-        for t in range(n - 1):
-            eln.print_matrix(xi[t], 'xi[t = %d]' % t)
-
         for i in range(self.hidden_states):
             self.initial[i] = gamma[0][i]
 
         gamma_sum = [math.nan] * self.hidden_states
         for i in range(self.hidden_states):
-            for t in range(1, n):
+            for t in range(n - 1):
                 gamma_sum[i] = eln.sum(gamma_sum[i], gamma[t][i])
 
         for i in range(self.hidden_states):
@@ -160,9 +165,10 @@ class Model:
                 self.transitions[i][j] = eln.product(sum_xi, -gamma_sum[i])
 
         for i in range(self.hidden_states):
-            gamma_sum[i] = eln.sum(gamma_sum[i], gamma[0][i])
+            gamma_sum[i] = eln.sum(gamma_sum[i], gamma[n - 1][i])
             gamma_actual = [math.nan] * self.emission_states
             for t in range(n):
                 gamma_actual[observations[t]] = eln.sum(gamma_actual[observations[t]], gamma[t][i])
             for j in range(self.emission_states):
                 self.emission[i][j] = eln.product(gamma_actual[j], -gamma_sum[i])
+        self.normalize()
--- 2/main.py
+++ 2/main.py
@@ -21,73 +21,95 @@ def get_intervals(seq):
 def main():
     parser = argparse.ArgumentParser(prog='MSA 2')
     parser.add_argument('-f', '--fasta', type=argparse.FileType('r'))
-    parser.add_argument('-u', '--human_output', type=argparse.FileType('w'))
     parser.add_argument('-o', '--output', type=argparse.FileType('w'))
     parser.add_argument('-m', '--hmm', type=argparse.FileType('r'))
     parser.add_argument('-s', '--segments', type=argparse.FileType('w'))
 
-    args = parser.parse_args(['-f', '1.fa', '-u', 'output.txt',
-                              '-o', 'output.csv', '-m', 'hmm1.txt', '-s', 'cg_segments.csv'])
+    args = parser.parse_args(['-f', 'vibrio.chrII.fa', '-o', 'output.txt', '-m', 'hmm.txt', '-s', 'cg_segments.csv'])
     input_seq = fa.read_fasta(args.fasta)[0][1]
-
     observations = [0 if nucl == 'A' or nucl == 'T' else 1 for nucl in input_seq]
-    # model = hmm.Model.load(args.hmm)
-    human_output = args.human_output
-    # viterbi(observations, model, human_output)
-    # viterbi_train(observations, model, human_output, args.segments)
 
-    model = hmm.Model.load(args.hmm)
-    baum_welch(observations, model, human_output)
+    hmm_file = args.hmm
+    model = hmm.Model.load(hmm_file)
+    output = args.output
+    viterbi(observations, model, output)
+    output.flush()
+    print('viterbi train')
+    viterbi_train(observations, model, output, args.segments)
+    output.flush()
+
+    print('baum')
+    hmm_file.seek(0)
+    model = hmm.Model.load(hmm_file)
+    baum_welch(observations, model, output)
+    print('Success')
 
 
+def viterbi(observations, model, output):
+    output.write('=============================\n')
+    output.write('========== Viterbi ==========\n')
+    output.write('=============================\n')
 
-def viterbi(observations, model, human_output):
     states, probability = model.viterbi(observations)
-    human_output.write('Viterbi\n')
-    human_output.write('Log probability: %f\n' % probability)
+    output.write('Log probability: %f\n' % probability)
 
     segments = get_intervals(states)
-    human_output.write('Segments: %d\n' % len(segments))
-    human_output.write('\n\n')
+    output.write('Segments: %d\n' % len(segments))
+    output.write('\n\n')
     return states
 
 
-def viterbi_train(observations, model, human_output, segments):
-    human_output.write('Viterbi train\n')
+def viterbi_train(observations, model, output, segments):
+    output.write('=============================\n')
+    output.write('======= Viterbi Train =======\n')
+    output.write('=============================\n')
 
     states, probability = model.viterbi(observations)
-    for i in range(10):
-        human_output.write('After iteration %d\n' % (i + 1))
+    previous_probability = math.nan
+    i = 0
+    while math.isnan(previous_probability) or previous_probability + 0.1 < probability:
+        i += 1
+        output.write('After iteration %d\n' % i)
+
         model.viterbi_train(states)
+        previous_probability = probability
         states, probability = model.viterbi(observations)
-        human_output.write('\tLog probability: %f\n' % probability)
-        human_output.write('\tSegments: %d\n' % len(get_intervals(states)))
-        human_output.write('\tTransitions:\n')
+        output.write('\tLog probability: %f\n' % probability)
+        output.write('\tSegments: %d\n' % len(get_intervals(states)))
+        output.write('\tTransitions:\n')
         for outer in model.transitions:
             for transition in outer:
-                human_output.write('\t%f' % eln.exp(transition))
-            human_output.write('\n')
+                output.write('\t\t%f' % eln.exp(transition))
+            output.write('\n')
+        output.flush()
 
     segments.write('no\tbegin\tend\n')
     i = 0
     for begin, end, value in get_intervals(states):
-        if value == 1:
-            segments.write('%d\t%d\t%d\n' % (i, begin, end))
-            i += 1
+        segments.write('%s\t%d\t%d\t%d\n' % ('CG' if value else 'AT', i, begin, end))
+        i += 1
+    segments.flush()
+    output.write('\n\n')
 
 
-def baum_welch(observations, model, human_output):
-    human_output.write('Baum-Welch\n')
-    alpha, beta, gamma = model.backward_forward(observations)
-    for i in range(3):
-        human_output.write('After iteration %d\n' % i)
-        probability = math.nan
-        for gamma_i in gamma[-1]:
-            probability = eln.sum(probability, gamma_i)
-        human_output.write('\tLog probability: %f\n' % probability)
-        model.baum_welch(observations, alpha, beta, gamma)
+def baum_welch(observations, model, output):
+    output.write('=============================\n')
+    output.write('======== Baum-Welch =========\n')
+    output.write('=============================\n')
+    alpha, beta, gamma, probability = model.backward_forward(observations)
+    previous_probability = math.nan
 
-    model.print(human_output)
+    i = 0
+    while math.isnan(previous_probability) or previous_probability + 0.1 < probability:
+        output.write('After iteration %d\n' % i)
+        i += 1
+        output.write('\tLog probability: %f\n' % probability)
+        model.print(output)
+        output.flush()
+        model.baum_welch(observations, alpha, beta, gamma, probability)
+
+        previous_probability = probability
+        alpha, beta, gamma, probability = model.backward_forward(observations)
 
 
 if __name__ == '__main__':
